88598f58b114818a3326d96183edbca2c14ac9b3
sapphi-red <green@sapphi.red>
2020-04-27 22:21:38 +0900

    sample2

--- samples/sample2.go
+++ samples/sample2.go
@@ -1,5 +1,7 @@
+// #2 おみくじの実装
 package main
 
+// 利用したい外部のコードを読み込む
 import (
 	"fmt"
 	"log"
@@ -14,10 +16,12 @@ import (
 
 const verifyToken = "00000000000000000000000000000000"
 
-// #2 おみくじの実装
+// main関数は最初に呼び出されることが決まっている
 func main() {
+	// ランダムな数値を利用する際のシード値の設定
 	rand.Seed(time.Now().UnixNano())
 
+	// LINEのAPIを利用する設定
 	bot, err := linebot.New(
 		os.Getenv("CHANNEL_SECRET"),
 		os.Getenv("CHANNEL_ACCESS_TOKEN"),
@@ -26,10 +30,13 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// Webhook endpoint
+	// LINEサーバからのリクエストを受け取ったときの処理
 	http.HandleFunc("/callback", func(w http.ResponseWriter, req *http.Request) {
 		fmt.Print("Accessed\n")
+
+		// リクエストを扱いやすい形に変換する
 		events, err := bot.ParseRequest(req)
+		// 変換に失敗したとき
 		if err != nil {
 			fmt.Println("ParseRequest error:", err)
 			if err == linebot.ErrInvalidSignature {
@@ -39,13 +46,19 @@ func main() {
 			}
 			return
 		}
+
+		// LINEサーバから来たメッセージによってやる処理を変える
 		for _, event := range events {
+			// LINEサーバのverify時は何もしない
 			if event.ReplyToken == verifyToken {
 				return
 			}
 
+			// メッセージが来たとき
 			if event.Type == linebot.EventTypeMessage {
+				// 返信を生成する
 				replyMessage := getReplyMessage(event)
+				// 生成した返信を送信する
 				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do(); err != nil {
 					log.Print(err)
 				}
@@ -53,10 +66,10 @@ func main() {
 		}
 	})
 
+	// LINEサーバからのリクエストを受け取る
 	if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
 		log.Fatal(err)
 	}
-
 }
 
 const helpMessage = `使い方
@@ -68,26 +81,32 @@ const helpMessage = `使い方
 それ以外:
 	それ以外にはまだ対応してないよ！ごめんね...`
 
+// 返信を生成する
 func getReplyMessage(event *linebot.Event) (replyMessage string) {
-
+	// 来たメッセージの種類によって分岐する
 	switch message := event.Message.(type) {
+	// テキストメッセージが来たとき
 	case *linebot.TextMessage:
+		// さらに「おみくじ」という文字列が含まれているとき
 		if strings.Contains(message.Text, "おみくじ") {
+			// おみくじ結果を取得する
 			return getFortune()
 		}
+		// そうじゃないときはオウム返しする
 		return message.Text
 
+	// スタンプが来たとき
 	case *linebot.StickerMessage:
 		replyMessage := fmt.Sprintf("sticker id is %s, stickerResourceType is %s", message.StickerID, message.StickerResourceType)
 		return replyMessage
 
+	// どっちでもないとき
 	default:
 		return helpMessage
-
 	}
-
 }
 
+// おみくじ結果の生成
 func getFortune() string {
 	oracles := map[int]string{
 		0: "大吉",
@@ -101,5 +120,6 @@ func getFortune() string {
 		8: "中凶",
 		9: "大凶",
 	}
+	// rand.Intn(10)は1～10のランダムな整数を返す
 	return oracles[rand.Intn(10)]
 }
