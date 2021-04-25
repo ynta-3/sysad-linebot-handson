6ee4f07044d2c78c1638a970486af13b160d4889
FUJISHIGE TEMMA <tenma.x0@gmail.com>
2021-04-26 08:27:16 +0900

    :recycle:refactor Step1

--- example/Step1.go
+++ example/Step1.go
@@ -1,3 +1,4 @@
+// #1 やまびこの実装
 package main
 
 // 利用したい外部のコードを読み込む
@@ -10,9 +11,11 @@ import (
 	"github.com/line/line-bot-sdk-go/v7/linebot"
 )
 
-const verifyToken = "00000000000000000000000000000000"
+const (
+	verifyToken = "00000000000000000000000000000000"
+)
 
-// main関数は最初に呼び出されることが決まっている
+// main関数は最初に呼び出されることがGo言語の仕様として決まっている
 func main() {
 	// LINEのAPIを利用する設定
 	bot, err := linebot.New(
@@ -25,41 +28,47 @@ func main() {
 
 	// LINEサーバからのリクエストを受け取ったときの処理
 	http.HandleFunc("/callback", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Print("Accessed\n")
+		log.Println("Accessed")
 
 		// リクエストを扱いやすい形に変換する
 		events, err := bot.ParseRequest(req)
+		switch err {
+		case nil:
 		// 変換に失敗したとき
-		if err != nil {
-			fmt.Println("ParseRequest error:", err)
-			if err == linebot.ErrInvalidSignature {
-				w.WriteHeader(400)
-			} else {
-				w.WriteHeader(500)
-			}
+		case linebot.ErrInvalidSignature:
+			log.Println("ParseRequest error:", err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		default:
+			log.Println("ParseRequest error:", err)
+			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
 
-		// LINEサーバから来たメッセージによってやる処理を変える
+		// LINEサーバから来たメッセージによって行う処理を変える
 		for _, event := range events {
-			// LINEサーバのverify時は何もしない
+			// LINEサーバからのverify時は何もしない
 			if event.ReplyToken == verifyToken {
 				return
 			}
 
+			switch event.Type {
 			// メッセージが来たとき
-			if event.Type == linebot.EventTypeMessage {
+			case linebot.EventTypeMessage:
 				// 返信を生成する
 				replyMessage := getReplyMessage(event)
 				// 生成した返信を送信する
 				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do(); err != nil {
 					log.Print(err)
 				}
+			// それ以外
+			default:
+				continue
 			}
 		}
 	})
 
-	// LINEサーバからのリクエストを受け取る
+	// LINEサーバからのリクエストを受け取るプロセスを起動
 	if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
 		log.Fatal(err)
 	}
@@ -74,8 +83,8 @@ const helpMessage = `使い方
 	それ以外にはまだ対応してないよ！ごめんね...`
 
 // 返信を生成する
-func getReplyMessage(event *linebot.Event) (replyMessage string) {
-	// 来たメッセージの種類によって分岐する
+func getReplyMessage(event *linebot.Event) string {
+	// 来たメッセージの種類によって行う処理を変える
 	switch message := event.Message.(type) {
 	// テキストメッセージが来たとき
 	case *linebot.TextMessage:
@@ -83,10 +92,8 @@ func getReplyMessage(event *linebot.Event) (replyMessage string) {
 
 	// スタンプが来たとき
 	case *linebot.StickerMessage:
-		replyMessage := fmt.Sprintf("sticker id is %s, stickerResourceType is %s", message.StickerID, message.StickerResourceType)
-		return replyMessage
-
-	// どっちでもないとき
+		return fmt.Sprintf("sticker id is %s, stickerResourceType is %s", message.StickerID, message.StickerResourceType)
+	// それ以外
 	default:
 		return helpMessage
 	}
