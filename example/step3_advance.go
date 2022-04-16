0a0a3463f426fa895d68a0a1c84cdc4673586c9a
xxarupakaxx <arupaka3tdigks@gmail.com>
2022-04-16 19:14:20 +0900

    add step3_advance.go

---
+++ example/step3_advance.go
@@ -0,0 +1,458 @@
+// #3 天気確認機能の実装
+package main
+
+// 利用したい外部のコードを読み込む
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/joho/godotenv"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/line/line-bot-sdk-go/v7/linebot"
+)
+
+// init関数はmain関数実行前の初期化のために呼び出されることがGo言語の仕様として決まっている
+func init() {
+	// ランダムな数値を生成する際のシード値の設定
+	rand.Seed(time.Now().UnixNano())
+}
+
+// main関数は最初に呼び出されることがGo言語の仕様として決まっている
+func main() {
+	// ここで.envファイル全体を読み込みます。
+	// この読み込み処理がないと、個々の環境変数が取得出来ません。
+	// 読み込めなかったら err にエラーが入ります。
+	err := godotenv.Load(".env")
+
+	// もし err がnilではないなら、"読み込み出来ませんでした"が出力されます。
+	if err != nil {
+		fmt.Printf("読み込み出来ませんでした: %v", err)
+	}
+
+	// LINEのAPIを利用する設定
+	bot, err := linebot.New(
+		os.Getenv("CHANNEL_SECRET"),
+		os.Getenv("CHANNEL_ACCESS_TOKEN"),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// LINEサーバからのリクエストを受け取ったときの処理
+	http.HandleFunc("/callback", func(w http.ResponseWriter, req *http.Request) {
+		log.Println("Accessed")
+
+		// リクエストを扱いやすい形に変換する
+		events, err := bot.ParseRequest(req)
+		if err != nil {
+			if err == linebot.ErrInvalidSignature {
+				w.WriteHeader(http.StatusBadRequest)
+			} else {
+				w.WriteHeader(http.StatusInternalServerError)
+			}
+			return
+		}
+
+		// LINEサーバから来たメッセージによって行う処理を変える
+		for _, event := range events {
+			switch event.Type {
+			// メッセージが来たとき
+			case linebot.EventTypeMessage:
+				// 返信を生成する
+				replyMessage := getReplyMessage(event)
+				// 生成した返信を送信する
+				if _, err = bot.ReplyMessage(event.ReplyToken, replyMessage).Do(); err != nil {
+					log.Print(err)
+				}
+			// それ以外
+			default:
+				continue
+			}
+		}
+	})
+
+	// LINEサーバからのリクエストを受け取るプロセスを起動
+	if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+const helpMessage = `使い方
+テキストメッセージ:
+	"おみくじ"がメッセージに入ってれば今日の運勢を占うよ！
+	それ以外はやまびこを返すよ！
+スタンプ:
+	スタンプの情報を答えるよ！
+位置情報:
+	その場所の天気・気温・湿度を答えるよ！
+それ以外:
+	それ以外にはまだ対応してないよ！ごめんね...`
+
+// 返信を生成する
+func getReplyMessage(event *linebot.Event) (replyMessage linebot.SendingMessage) {
+	// 来たメッセージの種類によって行う処理を変える
+	switch message := event.Message.(type) {
+	// テキストメッセージが来たとき
+	case *linebot.TextMessage:
+		// さらに「おみくじ」という文字列が含まれているとき
+		if strings.Contains(message.Text, "おみくじ") {
+			// おみくじ結果を取得する
+			return linebot.NewTextMessage(getFortune())
+		}
+		// それ以外のときはオウム返しする
+		return linebot.NewTextMessage(message.Text)
+
+	// スタンプが来たとき
+	case *linebot.StickerMessage:
+		return linebot.NewTextMessage(fmt.Sprintf("sticker id is %v, stickerResourceType is %v", message.StickerID, message.StickerResourceType))
+
+		// 位置情報が来たとき
+	case *linebot.LocationMessage:
+		// その場所の天気
+		replyMessage, err := getWeekWeather(message)
+		if err != nil {
+			log.Print(err)
+		}
+		return replyMessage
+
+	// それ以外のとき
+	default:
+		return linebot.NewTextMessage(helpMessage)
+	}
+}
+
+// おみくじ結果の生成
+func getFortune() string {
+	oracles := map[int]string{
+		0: "大吉",
+		1: "中吉",
+		2: "小吉",
+		3: "末吉",
+		4: "吉",
+		5: "凶",
+		6: "末凶",
+		7: "小凶",
+		8: "中凶",
+		9: "大凶",
+	}
+	// rand.Intn(10)は1～10のランダムな整数を返す
+	return oracles[rand.Intn(len(oracles))]
+}
+
+// 天気の情報で帰ってくる形式 (2)
+type Weather struct {
+	Icon string `json:"icon"` // 現状使わない
+}
+
+type WeekWeather struct {
+	Week []Daily `json:"daily"`
+}
+
+type Daily struct {
+	Temporary TempInfo  `json:"temp"`
+	Humidity  float32   `json:"humidity"`
+	Weathers  []Weather `json:"weather"`
+}
+
+type TempInfo struct {
+	Min float32 `json:"min"`
+	Max float32 `json:"max"`
+}
+
+func getWeekWeather(location *linebot.LocationMessage) (*linebot.FlexMessage, error) {
+	lat := strconv.FormatFloat(location.Latitude, 'f', 6, 64)
+	lon := strconv.FormatFloat(location.Longitude, 'f', 6, 64)
+	url := fmt.Sprintf("https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&exclude=current,minutely,hourly,alerts&appid=%s", lat, lon, os.Getenv("APP_ID"))
+	// OpenWeatherMapAPIへのリクエスト
+	res, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	// OpenWeatherMapAPIからのレスポンスを扱いやすい形に変換する
+	weatherData := WeekWeather{}
+	err = json.NewDecoder(res.Body).Decode(&weatherData)
+	if err != nil {
+		return nil, err
+	}
+	return CreateWeatherCarouseMessage(weatherData), nil
+}
+
+func CreateWeatherCarouseMessage(data WeekWeather) *linebot.FlexMessage {
+	resp := linebot.NewFlexMessage(
+		"Weather Information",
+		&linebot.CarouselContainer{
+			Type: linebot.FlexContainerTypeCarousel,
+			Contents: []*linebot.BubbleContainer{
+				{
+					Type:      linebot.FlexContainerTypeBubble,
+					Direction: linebot.FlexBubbleDirectionTypeLTR,
+					Header: &linebot.BoxComponent{
+						Type:   linebot.FlexComponentTypeBox,
+						Layout: linebot.FlexBoxLayoutTypeBaseline,
+						Contents: []linebot.FlexComponent{
+							&linebot.TextComponent{
+								Type:   linebot.FlexComponentTypeText,
+								Text:   "今日の天気",
+								Size:   linebot.FlexTextSizeTypeLg,
+								Align:  linebot.FlexComponentAlignTypeCenter,
+								Weight: linebot.FlexTextWeightTypeBold,
+								//Color:      "",
+								//Action:     nil,
+							},
+						},
+						CornerRadius: linebot.FlexComponentCornerRadiusTypeXxl,
+						BorderColor:  "#00bfff",
+						//Action: nil,
+					},
+					Hero: &linebot.ImageComponent{
+						Type:        linebot.FlexComponentTypeImage,
+						URL:         ConvertWeatherImage(data.Week[0].Weathers[0].Icon),
+						Size:        linebot.FlexImageSizeTypeXxl,
+						AspectRatio: linebot.FlexImageAspectRatioType1to1,
+						AspectMode:  linebot.FlexImageAspectModeTypeFit,
+						//Action:          nil,
+					},
+					Body: &linebot.BoxComponent{
+						Type:   linebot.FlexComponentTypeBox,
+						Layout: linebot.FlexBoxLayoutTypeVertical,
+						Contents: []linebot.FlexComponent{
+							&linebot.TextComponent{
+								Type: linebot.FlexComponentTypeText,
+								Text: "最高気温 : " + strconv.Itoa(int(data.Week[0].Temporary.Max - 273.15)) + "℃\n",
+								Flex: linebot.IntPtr(1),
+								Size: linebot.FlexTextSizeTypeXl,
+								Wrap: true,
+								//Action:     nil,
+								MaxLines: linebot.IntPtr(2),
+							},
+							&linebot.TextComponent{
+								Type: linebot.FlexComponentTypeText,
+								Text: "最低気温 : " + strconv.Itoa(int(data.Week[0].Temporary.Min - 273.15)) + "℃\n",
+								Flex: linebot.IntPtr(1),
+								Size: linebot.FlexTextSizeTypeXl,
+								Wrap: true,
+								//Action:     nil,
+								MaxLines: linebot.IntPtr(2),
+							},
+							&linebot.TextComponent{
+								Type: linebot.FlexComponentTypeText,
+								Text: fmt.Sprintf("湿度 : %.2f %%",data.Week[0].Humidity),
+								//Contents:   nil,
+								Flex: linebot.IntPtr(6),
+								Size: linebot.FlexTextSizeTypeSm,
+								Wrap: true,
+								//Color:      "",
+								//Action:     nil,
+								MaxLines: linebot.IntPtr(10),
+							},
+						},
+						BorderColor: "#5cd8f7",
+						//Action:          nil,
+					},
+					Styles: &linebot.BubbleStyle{
+						Header: &linebot.BlockStyle{
+							Separator:      true,
+							SeparatorColor: "#2196F3",
+						},
+						Hero: &linebot.BlockStyle{
+							Separator:      true,
+							SeparatorColor: "#2196F3",
+						},
+						Body: &linebot.BlockStyle{
+							Separator:      true,
+							SeparatorColor: "#37474F",
+						},
+						Footer: &linebot.BlockStyle{
+							Separator:      true,
+							SeparatorColor: "#2196F3",
+						},
+					},
+				},
+				{
+					Type:      linebot.FlexContainerTypeBubble,
+					Direction: linebot.FlexBubbleDirectionTypeLTR,
+					Header: &linebot.BoxComponent{
+						Type:   linebot.FlexComponentTypeBox,
+						Layout: linebot.FlexBoxLayoutTypeBaseline,
+						Contents: []linebot.FlexComponent{
+							&linebot.TextComponent{
+								Type:   linebot.FlexComponentTypeText,
+								Text:   "明日の天気",
+								Size:   linebot.FlexTextSizeTypeLg,
+								Align:  linebot.FlexComponentAlignTypeCenter,
+								Weight: linebot.FlexTextWeightTypeBold,
+								//Color:      "",
+								//Action:     nil,
+							},
+						},
+						CornerRadius: linebot.FlexComponentCornerRadiusTypeXxl,
+						BorderColor:  "#00bfff",
+						//Action: nil,
+					},
+					Hero: &linebot.ImageComponent{
+						Type:        linebot.FlexComponentTypeImage,
+						URL:         ConvertWeatherImage(data.Week[1].Weathers[0].Icon),
+						Size:        linebot.FlexImageSizeTypeXxl,
+						AspectRatio: linebot.FlexImageAspectRatioType1to1,
+						AspectMode:  linebot.FlexImageAspectModeTypeFit,
+						//Action:          nil,
+					},
+					Body: &linebot.BoxComponent{
+						Type:   linebot.FlexComponentTypeBox,
+						Layout: linebot.FlexBoxLayoutTypeVertical,
+						Contents: []linebot.FlexComponent{
+							&linebot.TextComponent{
+								Type: linebot.FlexComponentTypeText,
+								Text: "最高気温 : " + strconv.Itoa(int(data.Week[1].Temporary.Max - 273.15)) + "℃\n",
+								Flex: linebot.IntPtr(1),
+								Size: linebot.FlexTextSizeTypeXl,
+								Wrap: true,
+								//Action:     nil,
+								MaxLines: linebot.IntPtr(2),
+							},
+							&linebot.TextComponent{
+								Type: linebot.FlexComponentTypeText,
+								Text: "最低気温 : " + strconv.Itoa(int(data.Week[1].Temporary.Min - 273.15)) + "℃\n",
+								Flex: linebot.IntPtr(1),
+								Size: linebot.FlexTextSizeTypeXl,
+								Wrap: true,
+								//Action:     nil,
+								MaxLines: linebot.IntPtr(2),
+							},
+							&linebot.TextComponent{
+								Type: linebot.FlexComponentTypeText,
+								Text: fmt.Sprintf("湿度 : %.2f %%",data.Week[1].Humidity),
+								//Contents:   nil,
+								Flex: linebot.IntPtr(6),
+								Size: linebot.FlexTextSizeTypeSm,
+								Wrap: true,
+								//Color:      "",
+								//Action:     nil,
+								MaxLines: linebot.IntPtr(10),
+							},
+						},
+						BorderColor: "#5cd8f7",
+						//Action:          nil,
+					},
+					Styles: &linebot.BubbleStyle{
+						Header: &linebot.BlockStyle{
+							Separator:      true,
+							SeparatorColor: "#2196F3",
+						},
+						Hero: &linebot.BlockStyle{
+							Separator:      true,
+							SeparatorColor: "#2196F3",
+						},
+						Body: &linebot.BlockStyle{
+							Separator:      true,
+							SeparatorColor: "#37474F",
+						},
+						Footer: &linebot.BlockStyle{
+							Separator:      true,
+							SeparatorColor: "#2196F3",
+						},
+					},
+				},
+				{
+					Type:      linebot.FlexContainerTypeBubble,
+					Direction: linebot.FlexBubbleDirectionTypeLTR,
+					Header: &linebot.BoxComponent{
+						Type:   linebot.FlexComponentTypeBox,
+						Layout: linebot.FlexBoxLayoutTypeBaseline,
+						Contents: []linebot.FlexComponent{
+							&linebot.TextComponent{
+								Type:   linebot.FlexComponentTypeText,
+								Text:   "明後日の天気",
+								Size:   linebot.FlexTextSizeTypeLg,
+								Align:  linebot.FlexComponentAlignTypeCenter,
+								Weight: linebot.FlexTextWeightTypeBold,
+								//Color:      "",
+								//Action:     nil,
+							},
+						},
+						CornerRadius: linebot.FlexComponentCornerRadiusTypeXxl,
+						BorderColor:  "#00bfff",
+						//Action: nil,
+					},
+					Hero: &linebot.ImageComponent{
+						Type:        linebot.FlexComponentTypeImage,
+						URL:         ConvertWeatherImage(data.Week[2].Weathers[0].Icon),
+						Size:        linebot.FlexImageSizeTypeXxl,
+						AspectRatio: linebot.FlexImageAspectRatioType1to1,
+						AspectMode:  linebot.FlexImageAspectModeTypeFit,
+						//Action:          nil,
+					},
+					Body: &linebot.BoxComponent{
+						Type:   linebot.FlexComponentTypeBox,
+						Layout: linebot.FlexBoxLayoutTypeVertical,
+						Contents: []linebot.FlexComponent{
+							&linebot.TextComponent{
+								Type: linebot.FlexComponentTypeText,
+								Text: "最高気温 : " + strconv.Itoa(int(data.Week[2].Temporary.Max - 273.15)) + "℃\n",
+								Flex: linebot.IntPtr(1),
+								Size: linebot.FlexTextSizeTypeXl,
+								Wrap: true,
+								//Action:     nil,
+								MaxLines: linebot.IntPtr(2),
+							},
+							&linebot.TextComponent{
+								Type: linebot.FlexComponentTypeText,
+								Text: "最低気温 : " + strconv.Itoa(int(data.Week[2].Temporary.Min - 273.15)) + "℃\n",
+								Flex: linebot.IntPtr(1),
+								Size: linebot.FlexTextSizeTypeXl,
+								Wrap: true,
+								//Action:     nil,
+								MaxLines: linebot.IntPtr(2),
+							},
+							&linebot.TextComponent{
+								Type: linebot.FlexComponentTypeText,
+								Text: fmt.Sprintf("湿度 : %.2f %%",data.Week[2].Humidity),
+								//Contents:   nil,
+								Flex: linebot.IntPtr(6),
+								Size: linebot.FlexTextSizeTypeSm,
+								Wrap: true,
+								//Color:      "",
+								//Action:     nil,
+								MaxLines: linebot.IntPtr(10),
+							},
+						},
+						BorderColor: "#5cd8f7",
+						//Action:          nil,
+					},
+					Styles: &linebot.BubbleStyle{
+						Header: &linebot.BlockStyle{
+							Separator:      true,
+							SeparatorColor: "#2196F3",
+						},
+						Hero: &linebot.BlockStyle{
+							Separator:      true,
+							SeparatorColor: "#2196F3",
+						},
+						Body: &linebot.BlockStyle{
+							Separator:      true,
+							SeparatorColor: "#37474F",
+						},
+						Footer: &linebot.BlockStyle{
+							Separator:      true,
+							SeparatorColor: "#2196F3",
+						},
+					},
+				},
+			},
+		},
+	)
+
+	return resp
+}
+
+func ConvertWeatherImage(pngNumber string) string {
+	return fmt.Sprintf("https://openweathermap.org/img/w/%s.png",pngNumber)
+}
